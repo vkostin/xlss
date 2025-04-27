e93263d77370074ddd9bfb74a3886f7dbbf4e241
Viktor Kostin <victor.kostin@gmail.com>
2025-04-27 16:15:16 +0300

    Added Size

--- src/main/java/io/github/vkostin/xlss/geometry/Size.java
+++ src/main/java/io/github/vkostin/xlss/geometry/Size.java
@@ -1,7 +1,21 @@
 package io.github.vkostin.xlss.geometry;
 
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
+@Getter
 @RequiredArgsConstructor
 public class Size {
+    private final int width;
+    private final int height;
+
+    public Size(CellPosition tl, CellPosition br) {
+        this.width = br.getX() - tl.getX();
+        this.height = br.getY() - tl.getY();
+    }
+
+    @Override
+    public String toString() {
+        return width + "x" + height;
+    }
 }
